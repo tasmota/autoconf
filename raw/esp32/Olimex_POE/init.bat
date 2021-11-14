5ddfa2cab934f04b8d4951674700850ecafda26c
Stephan Hadinger <stephan.hadinger@gmail.com>
2021-11-14 11:37:29 +0100

    Added Olimex ESP32 POE

---
+++ raw/esp32/Olimex_POE/init.bat
@@ -0,0 +1,5 @@
+Template {"NAME":"Olimex ESP32-PoE","GPIO":[1,1,1,1,1,1,0,0,5536,1,1,1,0,0,5600,0,0,0,0,5568,0,0,0,0,0,0,0,0,1,1,32,1,1,0,0,4704],"FLAG":0,"BASE":1}
+Module 0
+EthType 0
+EthAddress 0
+EthClockMode 3
