69ca7b990109c9cef96d2217f46a58df2f52878f
Jacek Ziółkowski <jziolkowski@users.noreply.github.com>
2021-11-15 18:58:53 +0100

    moved the file to root, rewritten using os.walk (#1)
    * moved the file to root, rewritten using os.walk
    
    * fix conflict, make os.walk ignore dotfiles
    
    * fix dot-file removal
    
    * re-added zip list sorting

--- gen.py
+++ gen.py
@@ -1,38 +1,26 @@
-import os
 import json
-from zipfile import *
+import os
+from collections import defaultdict
+from zipfile import ZipFile, ZIP_STORED
 
-dir_src = os.path.join((os.path.dirname(os.path.realpath(__file__))), "raw")    # dir of this script
-dir_dst = os.path.abspath(dir_src + '/..')
+RAW_DIR = './raw'
+zips = defaultdict(list)
 
-# print(f"dir_src={dir_src}")
-# print(f"dir_dst={dir_dst}")
+for board_dir, _, files in os.walk(RAW_DIR):
+    if files := [f for f in files if not f.startswith('.')]:
+        a, _, arch, board = board_dir.split('/')
+        os.makedirs(arch, exist_ok=True)
+        print(f"Compressing {board} for {arch}")
 
-def zip_one(from_path, arch, from_name, to_dir):
-	zip_name = os.path.join(to_dir, from_name + "_autoconf.zip")
-	# print(f"zip_one({from_path}, {from_name}, {to_dir}), zip_name={zip_name}")
-	os.chdir(from_path)
-	print(f"Compressing {arch}/{from_name}")
-	with ZipFile(zip_name, mode="w", compression=ZIP_STORED, allowZip64=False, compresslevel=None, strict_timestamps=True) as myzip:
-		files = os.listdir(from_path)
-		for f in files:
-			if not f.startswith('.'):
-				myzip.write(f)
-				# print(f"Compressed: {f}")
-
-# scan a subdir like 'raw/esp32' and zip each directory
-def zip_all(from_dir, arch, to_dir):
-	zips = []
-	for d in sorted(os.listdir(from_dir)):
-		if os.path.isdir(os.path.join(from_dir, d)):
-			zip_one(os.path.join(from_dir, d), arch, d, to_dir)
-			zips.append(d)
-	with open(to_dir + "_manifest.json", "w", encoding='utf-8') as f:
-		json.dump({'zip':zips}, f, indent=None, separators=(',', ':'))
+        zipfile_name = os.path.join(arch, f"{board}_autoconf.zip")
+        with ZipFile(zipfile_name, mode="w", compression=ZIP_STORED, allowZip64=False, compresslevel=None,
+                     strict_timestamps=True) as myzip:
+            for file in files:
+                filepath = os.path.join(board_dir, file)
+                myzip.write(filepath, file)
+        zips[arch].append(board)
 
 
-for d in os.listdir(dir_src):
-	if os.path.isdir(os.path.join(dir_src, d)):
-		print(f"Compressing {d}")
-		zip_all(os.path.join(dir_src, d), d, os.path.join(dir_dst, d))
-	  #print(os.path.join(dir_src, d))
+for arch, zips_list in zips.items():
+    with open(f"{arch}_manifest.json", 'w') as manifest:
+        json.dump({"zip": sorted(zips_list)}, manifest, indent=None, separators=(',', ':'))
