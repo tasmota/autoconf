3fb2c1cc0f6486bca93315c46d487c1e75116040
Jacek Ziółkowski <jziolkowski@users.noreply.github.com>
2021-11-15 21:13:11 +0100

    Update gen.py

--- gen.py
+++ gen.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 from zipfile import ZipFile, ZIP_STORED
 
 RAW_DIR = './raw'
-zips = defaultdict(list)
+packages = defaultdict(list)
 
 for board_dir, _, files in os.walk(RAW_DIR):
     if files := [f for f in files if not f.startswith('.')]:
@@ -12,15 +12,15 @@ for board_dir, _, files in os.walk(RAW_DIR):
         os.makedirs(arch, exist_ok=True)
         print(f"Compressing {board} for {arch}")
 
-        zipfile_name = os.path.join(arch, f"{board}_autoconf.zip")
+        zipfile_name = os.path.join(arch, f"{board}.autoconf")
         with ZipFile(zipfile_name, mode="w", compression=ZIP_STORED, allowZip64=False, compresslevel=None,
                      strict_timestamps=True) as myzip:
             for file in files:
                 filepath = os.path.join(board_dir, file)
                 myzip.write(filepath, file)
-        zips[arch].append(board)
+        packages[arch].append(board)
 
 
-for arch, zips_list in zips.items():
-    with open(f"{arch}_manifest.json", 'w') as manifest:
-        json.dump({"zip": sorted(zips_list)}, manifest, indent=None, separators=(',', ':'))
+for arch, packages_list in packages.items():
+    with open(f"{arch}_manifest.json", "w") as manifest:
+        json.dump({"files": sorted(packages_list)}, manifest, indent=None, separators=(",", ":"))
