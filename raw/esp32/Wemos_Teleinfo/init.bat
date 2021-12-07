c89d758eef795b0be553e0580c28ec0efe6e2478
Charles <hallard04@free.fr>
2021-12-07 17:06:22 +0100

    create default setup

--- raw/esp32/Wemos_Teleinfo/init.bat
+++ raw/esp32/Wemos_Teleinfo/init.bat
@@ -1,2 +1,22 @@
 Template {"NAME":"Wemos Teleinfo","GPIO":[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1376,1,1,640,608,5632,1,1,0,1,0,0,0,1,1,1,1,1,1,1,1,1],"FLAG":0,"BASE":1}
 Module 0
+
+; All these parameters are saved onto flash device
+; so once configured, it's better to leave them in
+; file init.bat
+; ===================================================
+
+; Set Teleinfo in legacy (historique) mode at 1200 baud.
+EnergyConfig Historique	
+
+; Set Teleinfo in standard mode at 9600 baud.
+; EnergyConfig Standard	
+
+; Enable sending of all frames in raw mode
+EnergyConfig Full	
+
+; Send only each 1 on 10 frames received
+EnergyConfig Skip 9
+
+
+
