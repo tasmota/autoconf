ab9104ab15bab2a43667582f7afc893c723b335c
Charles <hallard04@free.fr>
2022-06-26 12:03:19 +0200

    Added OTA Url

--- raw/esp32/DenkyD4_V1.1/init.bat
+++ raw/esp32/DenkyD4_V1.1/init.bat
@@ -1,5 +1,6 @@
 Template {"NAME":"Denky D4 (v1.1)","GPIO":[32,0,0,0,1,0,0,0,0,1,1376,1,0,0,0,0,0,640,608,0,0,0,0,0,0,0,5632,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1}
 Module 0
+OtaUrl https://raw.githubusercontent.com/tasmota/install/main/firmware/unofficial/tasmota32-teleinfo.bin
 
 ; All these parameters are saved onto flash device
 ; so once configured, it's better to leave them in
@@ -17,5 +18,6 @@ EnergyConfig Full
 
 ; Send only each 1 on 10 frames received
 EnergyConfig Skip 9
+
 
 
