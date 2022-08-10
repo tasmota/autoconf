75d8bcbd2c6fca40a674b43238e709253f7b766b
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-08-10 17:51:53 +0200

    fix ota url

--- raw/esp32/Sonoff_ZBPro/init.bat
+++ raw/esp32/Sonoff_ZBPro/init.bat
@@ -1,3 +1,3 @@
 Template {"NAME":"Sonoff Zigbee Pro","GPIO":[0,0,576,0,480,0,0,0,0,1,1,5792,0,0,0,3552,0,320,5793,3584,0,640,608,32,0,0,0,0,0,1,0,0,0,0,0,0],"FLAG":0,"BASE":1}
 Module 0
-OtaUrl https://raw.githubusercontent.com/tasmota/install/main/firmware/unofficial/tasmota32-zbbrdgpro.bin
+OtaUrl http://ota.tasmota.com/tasmota32/tasmota32-zbbrdgpro.bin
