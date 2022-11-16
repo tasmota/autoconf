0431bede8850d910c9f0a15f13e144ef41b937a3
Stephan Hadinger <stephan.hadinger@gmail.com>
2022-11-16 18:15:51 +0100

    Added Athom_Addressable_LED

---
+++ raw/esp32/Athom_Addressable_LED/init.bat
@@ -0,0 +1,5 @@
+Template {"NAME":"Athom Adressable LED","GPIO":[32,1,224,1,1,1,1,1,1,1,1,7904,1,1377,1376,1,0,1,1,1,0,1088,1,1,0,0,0,0,7872,1,1,1,1,0,0,1],"FLAG":0,"BASE":1}
+Module 0
+SetOption13 1 ; Allow immediate action on single button press
+PowerOnState 1
+
