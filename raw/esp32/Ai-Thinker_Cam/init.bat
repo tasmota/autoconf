95d725f6526565a994a993c328636b28c53960f6
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-03-05 16:58:41 +0100

    Update init.bat

--- raw/esp32/Ai-Thinker_Cam/init.bat
+++ raw/esp32/Ai-Thinker_Cam/init.bat
@@ -1,2 +1,6 @@
 Template {"NAME":"AITHINKER CAM","GPIO":[4992,1,672,1,416,5088,1,1,1,6720,736,704,1,1,5089,5090,0,5091,5184,5152,0,5120,5024,5056,0,0,0,0,4928,576,5094,5095,5092,0,0,5093],"FLAG":0,"BASE":2}
 Module 0
+SetOption3 0 ; Disable MQTT
+SetOption56 1 ; Scan Wi-Fi network at restart for configured AP's
+SetOption57 1 ; Scan Wi-Fi network every 44 minutes for configured AP's
+Reset 3 ; Wifi Settings ReCalibration
