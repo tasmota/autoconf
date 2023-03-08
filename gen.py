f5804035a90116563592ddfe4e903af55388a114
Stephan Hadinger <stephan.hadinger@gmail.com>
2023-03-08 09:27:50 +0100

    Clone esp32 to esp32solo1

--- gen.py
+++ gen.py
@@ -2,6 +2,7 @@ import json
 import os
 from collections import defaultdict
 from zipfile import ZipFile, ZIP_STORED
+import shutil
 
 RAW_DIR = './raw'
 packages = defaultdict(list)
@@ -24,3 +25,6 @@ for board_dir, _, files in os.walk(RAW_DIR):
 for arch, packages_list in packages.items():
     with open(f"{arch}_manifest.json", "w") as manifest:
         json.dump({"files": sorted(packages_list, key=str.casefold)}, manifest, indent=None, separators=(",", ":"))
+
+shutil.copytree("esp32", "esp32solo1", dirs_exist_ok=True)
+shutil.copy2("esp32_manifest.json", "esp32solo1_manifest.json")
