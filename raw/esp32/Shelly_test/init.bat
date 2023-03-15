2f4dfd6a3a6b0b131c6de4884097ea8e84dae87b
Stephan Hadinger <stephan.hadinger@gmail.com>
2023-03-15 14:41:58 +0100

    Fixed

--- raw/esp32/Shelly_test/init.bat
+++
@@ -1,5 +0,0 @@
-; copy display.ini and mcp23x.dat in file-system
-br def extract(fname) try var f = open(autoconf._archive + "#" + fname) var content = f.read() f.close() var f2 = open(fname, 'w') f2.write(content) f2.close() except "io_error" as e,m tasmota.log("BRY: failed to extract: " + fname + ", " + m, 2) end end
-br extract("display.ini")
-br extract("mcp23x.dat")
-
