2f07d3093d5b9ce91a70bf7baf9c7fd3052c1b71
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-09-14 18:08:29 +0200

    Update init.bat

--- raw/esp32c3/Shelly_Plus1Mini/init.bat
+++ raw/esp32c3/Shelly_Plus1Mini/init.bat
@@ -1,3 +1,3 @@
 Br load("Shelly_Plus1Mini.autoconf#cp_bootloader.be") load("Shelly_Plus1Mini.autoconf#bootloader.be") global.bootloader().flash()
-; not yet known! Template {"NAME":"Shelly Plus1Mini","GPIO":[576,32,0,4736,0,224,3200,8161,0,0,192,0,0,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1,"CMND":"AdcParam1 2,5600,4700,3350"}
-; Module 0
+Template {"NAME":"Shelly Plus1Mini","GPIO":[576,32,0,4736,0,224,0,0,0,0,192,0,0,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1,"CMND":"AdcParam1 2,5600,4700,3350"}
+Module 0
