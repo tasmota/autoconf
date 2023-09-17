2560ee76089d3e810056582e712b7eed2bfd7ab3
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-09-17 17:45:36 +0200

    accidently deleted

---
+++ raw/esp32c3/Shelly_Plus1Mini/init.bat
@@ -0,0 +1,3 @@
+Br load("Shelly_Plus1Mini.autoconf#migrate_shelly.be")
+Template {"NAME":"Shelly Plus1Mini","GPIO":[320,0,0,0,0,0,0,224,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1,"CMND":"AdcParam1 2,5600,4700,3350"}
+Module 0
