723ef5fa46afc45aada216db359fd4dcbf21b24e
Stephan Hadinger <stephan.hadinger@gmail.com>
2023-09-17 16:27:07 +0200

    migration files for Shelly_PlusPMMini

--- raw/esp32c3/Shelly_PlusPMMini/init.bat
+++ raw/esp32c3/Shelly_PlusPMMini/init.bat
@@ -1,3 +1,3 @@
-Br load("Shelly_PlusPMMini.autoconf#cp_bootloader.be") load("Shelly_PlusPMMini.autoconf#bootloader.be") global.bootloader().flash()
+Br load("Shelly_PlusPMMini.autoconf#migrate_shelly.be")
 Template {"NAME":"Shelly PlusPMMini","GPIO":[576,32,0,4736,0,0,3200,8161,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1,"CMND":"AdcParam1 2,5600,4700,3350"}
 Module 0
