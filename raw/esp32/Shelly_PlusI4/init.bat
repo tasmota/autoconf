3804fcf59223cbd02c531861622e71650705d2c9
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-09-21 17:32:57 +0200

    Update init.bat

--- raw/esp32/Shelly_PlusI4/init.bat
+++ raw/esp32/Shelly_PlusI4/init.bat
@@ -1,3 +1,9 @@
 Br load("Shelly_PlusI4.autoconf#migrate_shelly.be")
-Template {"NAME":"Shelly Plus i4","GPIO":[0,0,0,0,0,0,0,0,192,0,193,0,0,0,0,0,0,0,0,0,0,0,195,194,0,0,0,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1,"CMND":"SwitchMode1 1 | SwitchMode2 1 | SwitchMode3 1 | SwitchMode4 1 | SwitchTopic 0 | SetOption114 1"}
+Template {"NAME":"Shelly Plus i4","GPIO":[0,0,0,0,0,0,0,0,192,0,193,0,0,0,0,0,0,0,0,0,0,0,195,194,0,0,0,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1}
 Module 0
+SwitchMode1 1
+SwitchMode2 1
+SwitchMode3 1
+SwitchMode4 1
+SwitchTopic 0
+SetOption114 1
