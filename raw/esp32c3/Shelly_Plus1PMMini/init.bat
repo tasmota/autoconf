1ab00a0003ccd0b2a32746111f1a1485c7ab289f
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-09-21 17:40:28 +0200

    Update init.bat

--- raw/esp32c3/Shelly_Plus1PMMini/init.bat
+++ raw/esp32c3/Shelly_Plus1PMMini/init.bat
@@ -1,3 +1,4 @@
 Br load("Shelly_Plus1PMMini.autoconf#migrate_shelly.be")
-Template {"NAME":"Shelly Plus1PMMini","GPIO":[576,32,0,4736,0,224,3200,8161,0,0,192,0,0,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1,"CMND":"AdcParam1 2,5600,4700,3350"}
+Template {"NAME":"Shelly Plus1PMMini","GPIO":[576,32,0,4736,0,224,3200,8161,0,0,192,0,0,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1}
 Module 0
+AdcParam1 2,5600,4700,3350
