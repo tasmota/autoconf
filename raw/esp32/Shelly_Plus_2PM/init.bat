33faaed78198d64087176e1464d65b142c5f3a8f
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-09-21 17:29:35 +0200

    Update init.bat

--- raw/esp32/Shelly_Plus_2PM/init.bat
+++ raw/esp32/Shelly_Plus_2PM/init.bat
@@ -1,3 +1,4 @@
 Br load("Shelly_Plus_2PM.autoconf#migrate_shelly.be")
-Template {"NAME":"Shelly Plus 2PM PCB v0.1.9","GPIO":[320,0,0,0,32,192,0,0,225,224,0,0,0,0,193,0,0,0,0,0,0,608,640,3458,0,0,0,0,0,9472,0,4736,0,0,0,0],"FLAG":0,"BASE":1,"CMND":"AdcParam1 2,10000,10000,3350"}
+Template {"NAME":"Shelly Plus 2PM PCB v0.1.9","GPIO":[320,0,0,0,32,192,0,0,225,224,0,0,0,0,193,0,0,0,0,0,0,608,640,3458,0,0,0,0,0,9472,0,4736,0,0,0,0],"FLAG":0,"BASE":1}
 Module 0
+AdcParam1 2,10000,10000,3350
