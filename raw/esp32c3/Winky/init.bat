46a2ec2047d55fc107eb1d5f19b80bea1210a194
Charles <hallard04@free.fr>
2023-11-03 20:59:23 +0100

    Winky enabled deepsleep and usb management (#32)
    * Added sample commands as reminder
    
    * set teleperiod and bootloop remove
    
    * manage sleep mode / usb

--- raw/esp32c3/Winky/init.bat
+++ raw/esp32c3/Winky/init.bat
@@ -3,25 +3,24 @@ Template {"NAME":"Winky","GPIO":[1,4704,1376,5632,4705,640,608,1,1,32,1,0,0,0,0,
 Module 0
 
 ; All these parameters are saved onto flash device
-; so once configured, it's better to leave them in
-; file init.bat
-; ===================================================
+; so once configured, you can change them afterward
+; in the file autoexec.be copied onto the filesystem
+; =====================================================
 
-# Set auto timezone
-;Backlog0 Timezone 99; TimeStd 0,0,10,1,3,60; TimeDst 0,0,3,1,2,120
+; Disable Boot Loop Detection
+SetOption65 1
 
-# Don't display energy sensors on WebUI
-;backlog0 WebSensor0 0 ; WebSensor1 0 ; WebSensor2 0 ; WebSensor3 0
+; Set Telemetry to 10s (deepsleep)
+TelePeriod 10
 
+; # Set auto timezone
+Backlog0 Timezone 99; TimeStd 0,0,10,1,3,60; TimeDst 0,0,3,1,2,120
 
-; Set Teleinfo in legacy (historique) mode at 1200 baud.
+; # Set Teleinfo in legacy (historique) mode at 1200 baud.
 EnergyConfig Historique	
 
-; Set Teleinfo in standard mode at 9600 baud.
-; EnergyConfig Standard	
-
-; Enable sending of all frames in raw mode
+; # Enable sending of all frames in raw mode
 EnergyConfig Full	
 
-; Send only each 1 on 10 frames received
+; # Send only each 1 on 10 frames received
 EnergyConfig Skip 9
