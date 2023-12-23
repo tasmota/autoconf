ab7b7f8c173cd42ea6b234f5ad759bbf10d3958f
Thomas Brasser <thomasbrasser@gmail.com>
2023-12-23 16:58:41 +0100

    Add Shelly Mini1G3, Mini1PMG3 and MiniPMG3 (#36)
    Co-authored-by: Thomas Brasser <thomas@brasser.family>

---
+++ raw/esp32c3/Shelly_MiniPMG3/init.bat
@@ -0,0 +1,5 @@
+Br load("Shelly_PlusPMMini.autoconf#migrate_shelly.be")
+Template {"NAME":"Shelly PlusPMMini","GPIO":[576,32,0,4736,0,0,3200,8161,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1}
+Module 0
+AdcParam1 2,5600,4700,3350
+so21 1
