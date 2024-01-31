938fcf7b526d0d97c0f2f7b7853578fbc2a9575f
sfromis <47082390+sfromis@users.noreply.github.com>
2024-01-31 16:16:55 +0100

    Update init.bat
    Adding Led, Button and internal temp from the templates respository

--- raw/esp32/Shelly_Plus_1/init.bat
+++ raw/esp32/Shelly_Plus_1/init.bat
@@ -1,3 +1,3 @@
 Br load("Shelly_Plus_1.autoconf#migrate_shelly.be")
-Template {"NAME":"Shelly Plus 1","GPIO":[0,0,0,0,192,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,224,0,0,0,0,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1}
+Template {"NAME":"Shelly Plus 1","GPIO":[288,0,0,0,192,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,32,224,0,0,0,0,0,4736,0,0,0,0,0,0,0],"FLAG":0,"BASE":1,"CMND":"AdcParam1 2,10000,10000,3350"}
 Module 0
