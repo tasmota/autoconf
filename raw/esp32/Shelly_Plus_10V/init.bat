9e1d9b5a5222f9e10b343f5775794b4389633dcd
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-04-13 15:00:51 +0200

    add Template config for ShellyPlus 10V

--- raw/esp32/Shelly_Plus_10V/init.bat
+++ raw/esp32/Shelly_Plus_10V/init.bat
@@ -1,3 +1,3 @@
 Br load("Shelly_Plus_10V.autoconf#migrate_shelly.be")
-Template {"NAME":"Shelly Plus 10V","GPIO":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1,"CMND":"AdcParam1 2,10000,10000,3350"}
+Template {"NAME":"Shelly Plus 10V","GPIO":[288,0,0,0,192,0,0,0,0,0,0,0,0,0,193,1,0,0,0,0,0,32,416,0,0,0,0,0,4736,0,0,0,0,0,0,0],"FLAG":0,"BASE":1}
 Module 0
