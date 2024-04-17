f4ce345691e5b1e447fb40df0b28f2844bad8e0b
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-04-17 15:33:09 +0200

    add GPIO config for Shelly_Plus_Uni

--- raw/esp32/Shelly_Plus_Uni/init.bat
+++ raw/esp32/Shelly_Plus_Uni/init.bat
@@ -1,3 +1,3 @@
 Br load("Shelly_Plus_Uni.autoconf#migrate_shelly.be")
-Template {"NAME":"Shelly Plus Uni","GPIO":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1,"CMND":"AdcParam1 2,10000,10000,3350"}
+Template {"NAME":"Shelly Plus Uni","GPIO":[0,0,0,0,0,0,0,0,0,1216,0,0,0,0,320,224,0,225,0,0,0,4864,0,0,0,0,0,0,0,0,0,0,4704,193,192,194],"FLAG":0,"BASE":1}
 Module 0
