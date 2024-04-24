48eeb79605314ba66821e91eea9577ae64e35e97
Beormund <75735592+Beormund@users.noreply.github.com>
2024-04-24 10:50:49 +0100

    Create init.bat

---
+++ raw/esp32/WT-SC01/init.bat
@@ -0,0 +1,6 @@
+Template {"NAME":"WT32-SC01","GPIO":[6210,1,1,1,1,1,0,0,1,704,736,768,0,0,640,608,1,800,1024,992,0,1,1,1,0,0,0,0,1,1,1,1,1,0,0,1],"FLAG":0,"BASE":1}
+Module 0
+I2CDriver36 0
+DisplayModel 17
+DisplayType 0
+DisplayDimmer 100
