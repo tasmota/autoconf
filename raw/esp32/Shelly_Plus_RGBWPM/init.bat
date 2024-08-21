59f7c48cb80a26efd209ced3664caa64610d4dfe
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-21 13:51:03 +0200

    add Shelly Plus RGBWPM

---
+++ raw/esp32/Shelly_Plus_RGBWPM/init.bat
@@ -0,0 +1,3 @@
+Br load("Shelly_Plus_RGBWPM.autoconf#migrate_shelly.be")
+Template {"NAME":"Shelly Plus RGBW PM User","GPIO":[1,0,0,0,419,0,0,0,0,0,544,0,0,0,0,1,0,0,1,0,0,416,417,418,0,0,0,0,0,4736,11264,11296,1,1,1,1],"FLAG":0,"BASE":1}
+Module 0
