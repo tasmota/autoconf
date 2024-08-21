a478ee68c9ec23d6a73318d2f9303c00531c304e
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-21 15:35:44 +0200

    add voltage and current settings to Shelly Plus RGBWPM

--- raw/esp32/Shelly_Plus_RGBWPM/init.bat
+++ raw/esp32/Shelly_Plus_RGBWPM/init.bat
@@ -1,3 +1,10 @@
 Br load("Shelly_Plus_RGBWPM.autoconf#migrate_shelly.be")
 Template {"NAME":"Shelly Plus RGBW PM User","GPIO":[1,0,0,0,419,0,0,0,0,0,544,0,0,0,0,1,0,0,1,0,0,416,417,418,0,0,0,0,0,4736,11264,11296,1,1,1,1],"FLAG":0,"BASE":1}
 Module 0
+AdcGpio33 32000,40000,3350
+AdcGpio34 1161,3472,10,30
+AdcGpio35 960,1017,0.01,0.706
+Fade 1
+Speed 6
+VoltRes 2
+WattRes 2
