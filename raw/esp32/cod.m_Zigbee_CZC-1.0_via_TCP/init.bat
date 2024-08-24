9150bce93330894a0d09f7cc8e2ccd658b6a3b60
Stephan Hadinger <stephan.hadinger@gmail.com>
2024-08-24 22:36:43 +0200

    add cod.m Zigbee TCP

---
+++ raw/esp32/cod.m_Zigbee_CZC-1.0_via_TCP/init.bat
@@ -0,0 +1,7 @@
+Template {"NAME":"cod.m Zigbee CZC-1.0 (via TCP)","GPIO":[0,0,1,0,5472,5536,0,0,576,1,288,1,0,0,5600,1,1,1,1,5568,1,1,1,1,0,0,0,0,0,1,1,32,5504,1,1,1],"FLAG":0,"BASE":1}
+Module 0
+EthType 0
+EthAddress 0
+EthClockMode 3
+Backlog rule1 on system#boot do TCPStart 8888 endon ; rule1 1 ; TCPStart 8888
+
