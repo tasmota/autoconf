874d1b7840846e4fbdfabd595f5a58172ca0a70f
Charles <hallard04@free.fr>
2024-09-09 23:14:48 +0200

    align teleinfo config to Winky
    align teleinfo config to Winky

--- raw/esp32/DenkyD4_V1.3/init.bat
+++ raw/esp32/DenkyD4_V1.3/init.bat
@@ -13,11 +13,20 @@ SetOption65 1
 ; define OTA Url
 OtaUrl https://github.com/NicolasBernaerts/tasmota/raw/master/teleinfo/binary/tasmota32-teleinfo-denkyd4.bin
 
-; # Set auto timezone
+; Set auto timezone
 Backlog0 Timezone 99; TimeStd 0,0,10,1,3,60; TimeDst 0,0,3,1,2,120
 
-; # Set Teleinfo in legacy (historique) mode at 1200 baud.
+; Set Teleinfo in legacy (historique) mode at 1200 baud.
 ; EnergyConfig Historique	
 
-; # Set Teleinfo in stadard mode at 9600 baud.
+; Set Teleinfo in standard mode at 9600 baud.
 ; EnergyConfig Standard
+
+; Set Teleinfo to autodetect mode (standard or historique)
+Energyconfig automode 
+
+; Set LED brightness to 75%, in sleep mode it will be bright/2
+Energyconfig bright=75
+
+; 0 for Green LED and 1 for Period Indicator (blue, white or red)
+Energyconfig period=1 
