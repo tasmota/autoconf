8ba22d1a4b35d12ead7f3d0f73e792b165c56572
Charles <hallard04@free.fr>
2024-09-10 17:07:06 +0200

    Winky Teleinfo new parameters (#53)
    * Added example of new configuration parameters
    
    Added example of new configuration parameters
    
    * enable auto detection of Standard or Legacy mode
    
    enable auto detection of Standard or Legacy mode
    default led to 75% brightness and period color
    
    * align teleinfo config to Winky
    
    align teleinfo config to Winky
    
    * secure device restart at the end of init.bat
    
    secure device restart at the end of init.bat

--- raw/esp32c3/Winky/init.bat
+++ raw/esp32c3/Winky/init.bat
@@ -1,5 +1,5 @@
 Br load("Winky.autoconf#cp2fs.be")
-Template {"NAME":"Winky","GPIO":[1,4704,1376,5632,4705,640,608,1,1,32,1,0,0,0,0,0,0,0,1,1,1,1],"FLAG":0,"BASE":1}
+Template {"NAME":"Winky C3","GPIO":[1,4704,1376,5632,4705,640,608,1,1,32,1,0,0,0,0,0,0,0,1,1,1,1],"FLAG":0,"BASE":1}
 Module 0
 
 ; All these parameters are saved onto flash device
@@ -19,16 +19,24 @@ TelePeriod 290
 ; Set Sleeping time
 DeepSleepTime 290
 
-
-
 ; define OTA Url
 OtaUrl https://github.com/NicolasBernaerts/tasmota/raw/master/teleinfo/binary/tasmota32c3-teleinfo-winky.bin
 
-; # Set auto timezone
+; Set auto timezone
 Backlog0 Timezone 99; TimeStd 0,0,10,1,3,60; TimeDst 0,0,3,1,2,120
 
-; # Set Teleinfo in legacy (historique) mode at 1200 baud.
+; Set Teleinfo in legacy (historique) mode at 1200 baud.
 ; EnergyConfig Historique	
 
-; # Set Teleinfo in stadard mode at 9600 baud.
+; Set Teleinfo in stadard mode at 9600 baud.
 ; EnergyConfig Standard
+
+; Set LED brightness to 75%, in sleep mode it will be bright/2
+Energyconfig bright=75
+
+; 0 for Green LED and 1 for Period Indicator (blue, white or red)
+Energyconfig period=1 
+
+; Set Teleinfo to autodetect mode (standard or historique)
+; this one restart device, use it at the end of file
+Energyconfig automode 
--- raw/esp32c6/Winky/init.bat
+++ raw/esp32c6/Winky/init.bat
@@ -22,11 +22,21 @@ DeepSleepTime 290
 ; define OTA Url
 OtaUrl https://github.com/NicolasBernaerts/tasmota/raw/master/teleinfo/binary/tasmota32c6-teleinfo-winky.bin
 
-; # Set auto timezone
+; Set auto timezone
 Backlog0 Timezone 99; TimeStd 0,0,10,1,3,60; TimeDst 0,0,3,1,2,120
 
-; # Set Teleinfo in legacy (historique) mode at 1200 baud.
+; Set Teleinfo in legacy (historique) mode at 1200 baud.
 ; EnergyConfig Historique	
 
-; # Set Teleinfo in stadard mode at 9600 baud.
+; Set Teleinfo in standard mode at 9600 baud.
 ; EnergyConfig Standard
+
+; Set LED brightness to 75%, in sleep mode it will be bright/2
+Energyconfig bright=75
+
+; 0 for Green LED and 1 for Period Indicator (blue, white or red)
+Energyconfig period=1 
+
+; Set Teleinfo to autodetect mode (standard or historique)
+; this one restart device, use it at the end of file
+Energyconfig automode 
