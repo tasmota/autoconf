f98f42ba01e7df65319c00cfc4a14e3a30d45fea
s-hadinger <49731213+s-hadinger@users.noreply.github.com>
2025-01-31 21:33:08 +0100

    Use Berry instead of pixels commands

--- raw/esp32c6/Waveshare_LCD_1.47/init.bat
+++ raw/esp32c6/Waveshare_LCD_1.47/init.bat
@@ -2,5 +2,8 @@
 ; Use LVGL build
 Template {"NAME":"Waveshare ESP32C6 LCD 1.47","GPIO":[1,1,1,1,6720,672,704,736,1376,1,1,1,1,1,768,800,1,1,1,1,1,1024,992,1,0,0,6210,0,0,0,0],"FLAG":0,"BASE":1}
 Module 0
-Pixels 1
-PixelType 2
+; We can't use Pixels and PixelType commands because the template is not yet active
+; so we use Berry helpers instead to emulate 'Pixels 1' and 'PixelType 2'
+Br tasmota.settings.light_pixels = 1
+Br tasmota.settings.light_pixels_order = 2
+Br tasmota.settings.light_pixels_rgbw = 0
