cad5852939c2e1d8791a0c76394e90a1c1f8a0a7
s-hadinger <49731213+s-hadinger@users.noreply.github.com>
2025-01-31 17:13:11 +0100

    Add Waveshare ESP32C6 LCD 1.47

---
+++ raw/esp32c6/Waveshare_LCD_1.47/init.bat
@@ -0,0 +1,6 @@
+; Waveshare ESP32C6 LCD 1.47
+; Use LVGL build
+Template {"NAME":"Waveshare ESP32C6 LCD 1.47","GPIO":[1,1,1,1,6720,672,704,736,1376,1,1,1,1,1,768,800,1,1,1,1,1,1024,992,1,0,0,6210,0,0,0,0],"FLAG":0,"BASE":1}
+Module 0
+Pixels 1
+PixelType 2
