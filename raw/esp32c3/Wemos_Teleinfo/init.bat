613c2d25003c71bca810a2209ef5563345d65131
Charles <hallard04@free.fr>
2026-04-11 18:59:57 +0200

    TELEINFO: WEMOS: uniform teleinfo init.bat for all device (#77)
    Uniform teleinfo configuration on all devices
    Change order of init command because some that require reset set a flag that can trigger reset 250ms after,  and if all is not finished, the end of init.bat is never executed thanks to @NicolasBernaerts

--- raw/esp32c3/Wemos_Teleinfo/init.bat
+++ raw/esp32c3/Wemos_Teleinfo/init.bat
@@ -1,19 +1,46 @@
-Template {"NAME":"Wemos Teleinfo","GPIO":[1,1,1376,1,5632,1,1,288,640,1,608,1,1,1,1376,1,1,640,1,0,1,1],"FLAG":0,"BASE":1}
+; Generic Teleinfo
+; ----------------
+
+; Reset settings, but preserve wifi, MQTT and FS
+InitDevice 6
+
+; Disable analog values display
+WebSensor2 0
+
+; Disable energy values display
+WebSensor3 0
+
+; Disable Boot Loop Detection
+SetOption65 1
+
+; Enable Wifi Scan (avoid wifi lost if router change channel)
+SetOption56 1
+
+; Set WebRefresh to 1 second
+WebRefresh 1000
+
+; Set auto timezone
+Backlog Timezone 99; TimeStd 0,0,10,1,3,60; TimeDst 0,0,3,1,2,120;
+
+; Set blinking LED color : 0 for Green LED and 1 for Period Indicator (blue, white or red)
+; Set Teleinfo to autodetect mode 
+Energyconfig period=1 reset
+
+; Wemos C3 Teleinfo
+; -----------------
+
+; Set module configuration
+Template {"NAME":"Wemos C3 Teleinfo","GPIO":[1,1,1376,1,5632,1,1,288,640,1,608,1,1,1,1376,1,1,640,1,0,1,1],"FLAG":0,"BASE":1}
+
+; Set LED brightness to 75%, in sleep mode it will be bright/2
+Energyconfig bright=75
+
+; OTA Url
+OtaUrl https://github.com/NicolasBernaerts/tasmota/raw/master/teleinfo/binary/tasmota32c3-teleinfo.bin
+
+; Set serial log output to info
+SerialLog 2
+
+; Set default module
 Module 0
 
-; All these parameters are saved onto flash device
-; so once configured, it's better to leave them in
-; file init.bat
-; ===================================================
-
-; Set Teleinfo in legacy (historique) mode at 1200 baud.
-EnergyConfig Historique	
-
-; Set Teleinfo in standard mode at 9600 baud.
-; EnergyConfig Standard	
-
-; Enable sending of all frames in raw mode
-EnergyConfig Full	
-
-; Send only each 1 on 10 frames received
-EnergyConfig Skip 9
