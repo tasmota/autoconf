72463e64d2ef1c125481033504349a9a58f232d2
Charles <hallard04@free.fr>
2026-04-11 16:58:27 +0200

    TELEINFO: ETHTINFO
    Cometic arrangement

--- raw/esp32/EthTinfo_V1.1/init.bat
+++ raw/esp32/EthTinfo_V1.1/init.bat
@@ -31,10 +31,7 @@ Energyconfig period=1 reset
 
 ; Set module configuration
 Template {"NAME":"EthTinfo (Olimex ESP32)","GPIO":[1,1,8864,1,0,1376,0,0,5536,1,8832,8800,0,0,5600,1,1,1,1,5568,1,1,1,1,0,0,0,0,1,1,32,1,5632,1,1,1],"FLAG":0,"BASE":1}
-Module 0
-EthType 0
-EthAddress 0
-EthClockMode 3
+Backlog Ethernet 1; EthType 0; EthAddress 0; EthClockMode 3;
 
 ; Set LED brightness to 75%, in sleep mode it will be bright/2
 Energyconfig bright=75
@@ -42,8 +39,8 @@ Energyconfig bright=75
 ; OTA Url
 OtaUrl https://github.com/NicolasBernaerts/tasmota/raw/master/teleinfo/binary/tasmota32-teleinfo-ethernet.bin
 
-; Set serial log output to info on uart0 (log to onboard USB/Serial)
-Backlog SerialConfig 8N1; Baudrate 115200; SerialLog 2;
+; Set serial log output to info
+SerialLog 2;
 
 ; Set default module
 Module 0
