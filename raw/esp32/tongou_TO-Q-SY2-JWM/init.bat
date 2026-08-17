bfe178c32c23388325a7ed6513520ef233188824
Alexey D. Filimonov <alexey@filimonic.net>
2026-08-17 09:19:26 +0300

    tongou: add SY2 (TO-Q-SY2-JWM) (#80)

---
+++ raw/esp32/tongou_TO-Q-SY2-JWM/init.bat
@@ -0,0 +1,4 @@
+Template {"NAME":"tongou SY2","GPIO":[32,1,9312,1,1,320,1,1,9313,8160,3200,544,1,1,1,1,0,1,1,1,0,0,1,1,0,0,0,0,1,1,4736,1,1,0,0,1],"FLAG":0,"BASE":1}
+Module 0
+AdcParam1 2,40000,100000,3950,0
+OtaUrl https://ota.tasmota.com/tasmota32/release/tasmota32.bin
