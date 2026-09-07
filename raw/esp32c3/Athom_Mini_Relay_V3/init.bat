8d757f53915a14b99bbe7d774bff8eacc55d7b7f
Aiden <37043404+tarontop@users.noreply.github.com>
2026-09-07 16:32:37 +0800

    Add Athom USB Switch (#81)

--- raw/esp32c3/Athom_Mini_Relay_V3/init.bat
+++ raw/esp32c3/Athom_Mini_Relay_V3/init.bat
@@ -1,4 +1,7 @@
-Template {"NAME":"Athom Mini Relay V3","GPIO":[0,0,0,32,160,0,224,576,0,0,0,0,0,0,0,0,0,0,0,0,3104,0],"FLAG":0,"BASE":1}
+Template {"NAME":"Athom Mini Relay V3","GPIO":[0,0,0,32,160,0,224,320,0,0,0,0,0,0,0,0,0,0,0,0,3104,0],"FLAG":0,"BASE":1}
 Module 0
-SetOption21 1
+LedState 1
+LedMask 1
+SetOption31 0
+SetOption3 0
 PowerDelta 101
---
+++ raw/esp32c3/Athom_USB_Switch/init.bat
@@ -0,0 +1,6 @@
+Template {"NAME":"Athom USB Switch","GPIO":[0,0,0,288,0,0,32,0,0,0,224,0,0,0,0,0,0,0,0,0,0,0],"FLAG":0,"BASE":1}
+Module 0
+LedState 1
+LedMask 1
+SetOption31 0
+SetOption3 0
